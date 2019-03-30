329f4f65986cc8749885931dcfa1cf3b07196736
Orangehaswing_PC <673556024@qq.com>
2019-03-30 22:29:58 +0800

    java commen tools

---
+++ wingtool-parent/wingtoolparent/wingtool-system/src/test/java/cn/hutool/system/SystemUtilTest.java
@@ -0,0 +1,26 @@
+package cn.hutool.system;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SystemUtilTest {
+	
+	@Test
+	@Ignore
+	public void dumpTest() {
+		SystemUtil.dumpSystemInfo();
+	}
+	
+	@Test
+	public void getCurrentPidTest() {
+		long pid = SystemUtil.getCurrentPID();
+		Assert.assertTrue(pid > 0);
+	}
+	
+	@Test
+	public void getJavaInfoTest() {
+		JavaInfo javaInfo = SystemUtil.getJavaInfo();
+		Assert.assertNotNull(javaInfo);
+	}
+}
